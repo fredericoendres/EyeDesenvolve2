fe71a17df084363464724a58e562cb33c8c7a9fc
fredericoendres <116963749+fredericoendres@users.noreply.github.com>
2023-09-05 21:18:21 -0300

    Implementados botoes de dropdown

--- app/src/main/java/com/example/eyedesenvolve/RegistroActivity.java
+++ app/src/main/java/com/example/eyedesenvolve/RegistroActivity.java
@@ -1,14 +1,36 @@
 package com.example.eyedesenvolve;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    String[] EVENTOS = {"Eye Desenvolve 2023", "Startup Summit 2023", "Futurecom 2023"};
+    String[] GENEROS = {"Masculino", "Feminino", "Outro"};
+
+    AutoCompleteTextView autoCompleteTextViewEvento;
+    AutoCompleteTextView autoCompleteTextViewGenero;
+    ArrayAdapter<String> adapterEventos;
+    ArrayAdapter<String> adapterGeneros;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        autoCompleteTextViewEvento = findViewById(R.id.select_evento);
+        adapterEventos = new ArrayAdapter<String>(this,R.layout.list_dropdown, EVENTOS);
+        autoCompleteTextViewEvento.setAdapter(adapterEventos);
+
+        autoCompleteTextViewGenero = findViewById(R.id.select_genero);
+        adapterGeneros = new ArrayAdapter<String>(this, R.layout.list_dropdown, GENEROS);
+        autoCompleteTextViewGenero.setAdapter(adapterGeneros);
+
+
     }
+
+
 }
